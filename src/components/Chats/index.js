458023d94b0f22bf34545a484fc0c936cd7b0871
codelavkush <mauryalavkush19@gmail.com>
2025-06-04 16:09:38 +0530

    Add Chat view component

--- src/components/Chats/index.js
+++ src/components/Chats/index.js
@@ -2,10 +2,12 @@ import Users from "./Users";
 import ListChats from "./ListChats";
 import CreateChatForm from "./CreateChatForm";
 import ChatMessages from "./ChatMessages";
+import ChatView from "./ChatView";
 
 export {
     Users,
     ListChats,
     CreateChatForm,
     ChatMessages,
+    ChatView,
 }
